ddf55d44b3862ca949b0027eaf78a6b978eb3cb3
Remi Henriot <rhenriot@adagio.io>
2024-10-04 14:11:39 +0200

    Using the dimension type field instead of the name for the internal mapping

--- dist/js-indexus-sdk.cjs.js
+++ dist/js-indexus-sdk.cjs.js
@@ -1404,8 +1404,8 @@ const DIMENSIONS = {
   linear: Linear,
 };
 
-function mapDimension(name, args) {
-  return new DIMENSIONS[name](args);
+function mapDimension(type, args) {
+  return new DIMENSIONS[type](args);
 }
 
 const MASKS = {
@@ -1414,16 +1414,16 @@ const MASKS = {
   static: Static,
 };
 
-function mapMask(name, args) {
-  return new MASKS[name](args);
+function mapMask(type, args) {
+  return new MASKS[type](args);
 }
 
 const OFFSETS = {
   basic: Basic$1,
 };
 
-function mapOffset(name, args) {
-  return new OFFSETS[name](args);
+function mapOffset(type, args) {
+  return new OFFSETS[type](args);
 }
 
 class Collection extends Collection$1 {
@@ -1434,7 +1434,7 @@ class Collection extends Collection$1 {
     this._dimensions = [];
 
     const m = dimensions.reduce((total, dimension) => {
-      const d = mapDimension(dimension.name, dimension.args);
+      const d = mapDimension(dimension.type, dimension.args);
       this._dimensions.push(d);
       return total + d.pointLength();
     }, 0);
