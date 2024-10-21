a2385713242bff74bbf7c0a55ce3bdb797341d34
Remi Henriot <henriot.rem@gmail.com>
2024-10-21 22:05:27 +0200

    Exporting the Set class from the package

--- dist/js-indexus-sdk.umd.js
+++ dist/js-indexus-sdk.umd.js
@@ -815,6 +815,40 @@
     }
   }
 
+  class Set extends Set$1 {
+    constructor(collection, hash, count) {
+      super();
+
+      this._collection = collection;
+      this._hash = hash;
+      this._count = count;
+    }
+
+    /**
+     * Returns the name of the collection this element belongs to.
+     * @returns {string}
+     */
+    collection() {
+      return this._collection;
+    }
+
+    /**
+     * Returns the unique hash identifier of this element.
+     * @returns {string}
+     */
+    hash() {
+      return this._hash;
+    }
+
+    /**
+     * Returns the count of items associated with this element.
+     * @returns {number}
+     */
+    count() {
+      return this._count;
+    }
+  }
+
   // Point class (implements Point)
   class Point$1 extends Point$2 {
     constructor(latitude, longitude) {
@@ -1676,40 +1710,6 @@
       this.final = true;
       this.waiting = 0;
       this.target = 0;
-    }
-  }
-
-  class Set extends Set$1 {
-    constructor(collection, hash, count) {
-      super();
-
-      this._collection = collection;
-      this._hash = hash;
-      this._count = count;
-    }
-
-    /**
-     * Returns the name of the collection this element belongs to.
-     * @returns {string}
-     */
-    collection() {
-      return this._collection;
-    }
-
-    /**
-     * Returns the unique hash identifier of this element.
-     * @returns {string}
-     */
-    hash() {
-      return this._hash;
-    }
-
-    /**
-     * Returns the count of items associated with this element.
-     * @returns {number}
-     */
-    count() {
-      return this._count;
     }
   }
 
@@ -8439,6 +8439,7 @@
   exports.Locality = Locality;
   exports.Network = Network;
   exports.Peer = Peer;
+  exports.Set = Set;
   exports.Space = Space;
   exports.Spherical = Spherical;
   exports.decodeUrl64 = decodeUrl64;
