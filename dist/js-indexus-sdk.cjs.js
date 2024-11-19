7f6f0ce2490f195bfde973e9c42047b3cd7ea61c
Remi Henriot <rhenriot@adagio.io>
2024-11-19 17:30:56 +0100

    Improve the grouping method in Progressive search to consolidate with items

--- dist/js-indexus-sdk.cjs.js
+++ dist/js-indexus-sdk.cjs.js
@@ -2246,11 +2246,27 @@ function aggregate(data) {
   return results;
 }
 
-async function run(selected = { "@": true }, items = []) {
+async function run(selected = { "@": true }, depth = 0, items = []) {
   const sets = [];
   const selectedList = Object.keys(selected);
+  const merged = {};
 
   selected = {};
+
+  const consolidate = (elm) => {
+    const hash = elm._hash.substring(0, depth);
+    const set = merged[hash];
+
+    if (!set) {
+      merged[hash] = new Set(elm._collection, hash, 1, elm._metrics);
+      return;
+    }
+
+    set._count++;
+    set._metrics = set._metrics.map(
+      (metric, index) => metric + elm._metrics[index]
+    );
+  };
 
   const processElement = async (element) => {
     try {
@@ -2261,12 +2277,16 @@ async function run(selected = { "@": true }, items = []) {
           elm._parent = element;
           elm._bounds = this.space.decode(elm._hash);
 
-          if (
-            this.space.overlap(this.space.newSegment(this.bounds), elm._bounds)
-          ) {
-            if (elm instanceof Item$2) {
+          const overlap = this.space.overlap(
+            this.space.newSegment(this.bounds),
+            elm._bounds
+          );
+
+          if (overlap) {
+            if (elm instanceof Item$1) {
               items.push(elm);
-            } else if (elm instanceof Set$1) {
+              consolidate(elm);
+            } else if (elm instanceof Set) {
               sets.push(elm);
               selected[elm._hash] = true;
             }
@@ -2278,27 +2298,16 @@ async function run(selected = { "@": true }, items = []) {
     }
   };
 
-  await asyncPool(this.option.concurrencyLimit, selectedList, processElement);
+  await asyncPool(this.option.concurrency, selectedList, processElement);
 
-  const grouped = this.aggregate(sets);
+  const grouped = this.aggregate([...sets, ...Object.values(merged)]);
 
   if (
     sets.length > 0 &&
     grouped[this.option.dimension].length < this.option.resolution
   ) {
-    return await this.run(selected, items);
+    return await this.run(selected, depth + 1, items);
   }
-
-  items.forEach((item) => {
-    sets.forEach((set) => {
-      if (item._hash.startsWith(set._hash)) {
-        set._count--;
-        set._metrics = set._metrics.map(
-          (metric, index) => metric - item._metrics[index]
-        );
-      }
-    });
-  });
 
   return { items, sets, grouped };
 }
