393766a0d5b0624787195ab2f9fec0c2807c4ad8
Remi Henriot <henriot.rem@gmail.com>
2024-12-19 09:56:17 +0100

    Adding the xyz information in the aggregated elements

--- dist/js-indexus-sdk.esm.js
+++ dist/js-indexus-sdk.esm.js
@@ -2535,13 +2535,15 @@ function aggregate(data) {
     max += dimension.pointLength();
 
     data.forEach((elm) => {
-      const key = [
-        elm.xyz.resolution,
-        ...elm.xyz.coordinates.slice(min, max),
-      ].join("-");
+      const xyz = {
+        resolution: elm.xyz.resolution,
+        coordinates: elm.xyz.coordinates.slice(min, max),
+      };
+      const key = this.key(xyz);
 
       if (!groups.has(key)) {
         groups.set(key, {
+          xyz: xyz,
           bounds: elm.bounds[i],
           count: 0,
           metrics: Array(elm.metrics.length).fill(0),
