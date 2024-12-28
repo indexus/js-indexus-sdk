5bae042fd3966c1df0b8f3a15019051eca723402
Remi Henriot <rhenriot@adagio.io>
2024-12-28 21:32:42 +0100

    Change the aggregated Map returned in grid mode  into an object of values

--- dist/js-indexus-sdk.umd.js
+++ dist/js-indexus-sdk.umd.js
@@ -2586,7 +2586,7 @@
       max = 0;
 
     this.space.dimensions.forEach((dimension, i) => {
-      const groups = new Map();
+      const groups = {};
 
       min = max;
       max += dimension.pointLength();
@@ -2598,16 +2598,16 @@
         };
         const key = this.key(xyz);
 
-        if (!groups.has(key)) {
-          groups.set(key, {
+        if (!groups[key]) {
+          groups[key] = {
             xyz: xyz,
             bounds: elm.bounds[i],
             count: 0,
             metrics: Array(elm.metrics.length).fill(0),
-          });
+          };
         }
 
-        const group = groups.get(key);
+        const group = groups[key];
         group.count += elm.count;
         elm.metrics.forEach((m, idx) => {
           group.metrics[idx] += m;
